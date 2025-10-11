d54d839310f871dfd691def99f0de0c220aa2e00
Kamil Koziol <koziol.kamil@protonmail.com>
2025-10-11 22:18:48 +0200

    web: add Searchbar component for issues search

---
+++ web/src/lib/components/Searchbar.svelte
@@ -0,0 +1,28 @@
+<script>
+  import SearchIcon from '$lib/assets/icons/SearchIcon.svelte';
+</script>
+
+<div class="searchHolder">
+  <SearchIcon width="18px" title="Search" />
+  <input type="text" id="searchbar" placeholder="Search issues..." />
+</div>
+
+<style>
+  .searchHolder {
+    display: flex;
+    align-items: center;
+    background-color: #f1f3f4;
+    padding: 2px 8px;
+    border-radius: 10px;
+  }
+
+  input {
+    all: unset;
+    min-width: 60vw;
+    padding: 4px 12px;
+    border: unset;
+    background-color: #f1f3f4;
+    font-size: 14px;
+    color: #333;
+  }
+</style>
