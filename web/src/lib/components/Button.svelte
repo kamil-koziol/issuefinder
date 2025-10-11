0d0db4ce841df84c77061a672e759d54d39ea2a5
Kamil Koziol <koziol.kamil@protonmail.com>
2025-10-11 19:13:40 +0200

    web: add Button component

---
+++ web/src/lib/components/Button.svelte
@@ -0,0 +1,15 @@
+<button {...$$props}>
+  <slot />
+</button>
+
+<style>
+  button {
+    all: unset;
+    color: white;
+    background-color: var(--primary);
+    padding: 8px 18px;
+    border-radius: 8px;
+    cursor: pointer;
+    font-size: 14px;
+  }
+</style>
