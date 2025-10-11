5dfd7e59f030b2bd41804983c139a31a47346311
Kamil Koziol <koziol.kamil@protonmail.com>
2025-10-11 22:17:59 +0200

    web: add Logo

---
+++ web/src/lib/assets/icons/Logo.svelte
@@ -0,0 +1,23 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 60 20"
+  role="img"
+  aria-label="IssueFinder icon"
+  {...$$props}
+>
+  <title>IssueFinder icon</title>
+  <g fill="currentColor">
+    <circle cx="10" cy="10" r="6" />
+    <circle cx="30" cy="10" r="6" />
+    <circle cx="50" cy="10" r="6" />
+  </g>
+  <line
+    x1="16"
+    y1="10"
+    x2="44"
+    y2="10"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+  />
+</svg>
